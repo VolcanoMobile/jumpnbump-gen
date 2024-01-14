62e10e28ae1094754e79a5e8a375bfb239ce118e
Philippe Simons <simons.philippe@gmail.com>
2024-01-14 17:57:07 +0100

    Update bank.h

--- inc/bank.h
+++ inc/bank.h
@@ -6,17 +6,11 @@
 typedef struct __Bank_t Bank;
 
 Bank *         BANK_create(u16 _count, u16 _size);
-
 void           BANK_delete(Bank *_bank);
-
 void *         BANK_allocate(Bank *_bank);
-
 void           BANK_free(Bank *_bank, void *_cell);
-
 void           BANK_clear(Bank *_bank);
-
 void *         BANK_getFirst(const Bank *_bank);
-
 void *         BANK_getNext(const Bank *_bank, const void *_cell);
 
 #endif
